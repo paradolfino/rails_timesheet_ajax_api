0c206d4af8b3f35531ea37a9aa242d71f7a34bff
viktharien <viktharien@zoho.com>
2018-08-25 17:36:09 -0500

    use resource generator for posts

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+
+  validates :date, presence: true
+  validates :rationale, presence: true
+
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "creation" do
+    before do
+      @post = Post.create(date: Date.today, rationale: "anything")
+    end
+
+    it "can be created" do
+      expect(@post).to be_valid
+    end
+
+    it "cannot be created without a date and rationale" do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to be_invalid
+    end
+
+  end
+end
