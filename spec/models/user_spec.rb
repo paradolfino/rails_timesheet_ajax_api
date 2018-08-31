2c9fa6870d1bd82ba7162f8f833f50c3000e6a49
viktharien <viktharien@zoho.com>
2018-08-30 21:14:07 -0500

    implement custom full_name method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Snow")
+  end
   describe "creation" do
-    before do
-      @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Snow")
-    end
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -12,6 +12,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to be_invalid
+    end
+  end
+
+  describe "custom name methods" do
+    it "has a full_name method that combines first and last name" do
+      expect(@user.full_name).to eq("#{@user.last_name.upcase}, #{@user.first_name.upcase}")
     end
   end
 end
