2f0eb09d1582785942e1a710685c8f294535a259
viktharien <viktharien@zoho.com>
2018-09-01 18:40:47 -0500

    implement edit and update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all
@@ -17,11 +17,23 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-      redirect_to @post, notice: "Your post was created successfully!s"
+      redirect_to @post, notice: "Your post was created successfully!"
     else
       render 'new'
     end
 
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Your post was updated successfully!"
+    else
+      render 'edit'
+    end
   end
 
   private
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Snow")
+    # @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Snow")
+    @user = create(:user)
     login_as @user, :scope => :user
   end
   describe 'index' do
@@ -19,10 +20,12 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
-      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+      # post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      # # post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+      create(:post, user_id: @user.id)
+      create(:second_post, user_id: @user.id)
       visit posts_path
-      expect(page).to have_content(/Post1|Post2/)
+      expect(page).to have_content(/Some Rationale|Some more content/)
     end
   end
 
@@ -49,6 +52,29 @@ describe 'navigate' do
 
       click_on 'Submit'
       expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
+
+  describe 'edit' do
+    before do
+      @post = create(:post, user_id: @user.id)
+    end
+    it 'can be reached by edit on index page' do
+
+      visit posts_path
+      click_link "edit_#{@post.id}"
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be edited' do
+      visit edit_post_path(@post)
+
+      fill_in 'post[date]', with: "#{Date.today}"
+      fill_in 'post[rationale]', with: 'Edited Content'
+
+      click_on 'Submit'
+
+      expect(page).to have_content('Edited Content')
     end
   end
 end
