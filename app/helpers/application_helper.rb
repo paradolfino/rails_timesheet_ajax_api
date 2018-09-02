ff01e09ccf98e342b5560ee34c62bf8fa5d22c0d
viktharien <viktharien@zoho.com>
2018-09-01 20:09:13 -0500

    add new post button to nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  def nav_link(text, path)
-    link_to(text, path, class: "nav-link #{request.path == path ? "active" : nil}")
+  def nav_link(text, path, id="")
+    link_to(text, path, class: "nav-link #{request.path == path ? "active" : nil}", id: id)
   end
 end
