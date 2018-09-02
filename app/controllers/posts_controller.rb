dacd3b4fbe2f80f76f9d307d2bbf21daa208b2d5
viktharien <viktharien@zoho.com>
2018-09-01 20:25:16 -0500

    implement ability delete post records

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -34,6 +34,15 @@ class PostsController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def delete
+
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "Your post was deleted!"
   end
 
   private
